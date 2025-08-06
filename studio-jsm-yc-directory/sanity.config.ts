44d7e092141e49f464bcbd4ffa12603e5a2b7659
ammarAlzaqam <git@ammaralzaqam.com>
2025-08-06 10:57:21 +0300

    sanity schema

--- studio-jsm-yc-directory/sanity.config.ts
+++ studio-jsm-yc-directory/sanity.config.ts
@@ -2,15 +2,16 @@ import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemaTypes'
+import {markdownSchema} from 'sanity-plugin-markdown'
 
 export default defineConfig({
   name: 'default',
   title: 'JSM YC Directory',
 
-  projectId: "0o5f0wwj",
-  dataset:'production',
+  projectId: '0o5f0wwj',
+  dataset: 'production',
 
-  plugins: [structureTool(), visionTool()],
+  plugins: [structureTool(), visionTool(), markdownSchema()],
 
   schema: {
     types: schemaTypes,
--- studio-jsm-yc-directory/schemaTypes/index.ts
+++ studio-jsm-yc-directory/schemaTypes/index.ts
@@ -1,1 +1,4 @@
-export const schemaTypes = []
+import {authorType} from './author'
+import {startupType} from './startup'
+
+export const schemaTypes = [authorType, startupType]
