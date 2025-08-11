a182e76f2ffd1bbb151d0fd5c1e47e47c86495dd
ammarAlzaqam <git@ammaralzaqam.com>
2025-08-11 11:26:23 +0300

    Add View component

---
+++ yc-directory/components/View.tsx
@@ -0,0 +1,34 @@
+import { client } from "@/sanity/client";
+import Ping from "./Ping";
+import { STARTUP_VIEWS_QUERY } from "@/sanity/queries";
+import { formateNumber } from "@/utils/formateNumber";
+import { writeClient } from "@/sanity/write-client";
+import { after } from "next/server";
+
+export default async function View({ id }: { id: string }) {
+  const { views: totalViews } = await client
+    .withConfig({ useCdn: false })
+    .fetch(STARTUP_VIEWS_QUERY, { id });
+
+  // TODO: Update the number of views
+
+  after(
+    async () =>
+      await writeClient
+        .patch(id)
+        .set({ views: totalViews + 1 })
+        .commit()
+  );
+
+  return (
+    <div className="view-container">
+      <div className="absolute -top-2 -right-2">
+        <Ping />
+      </div>
+
+      <p className="view-text">
+        <span className="font-black">{formateNumber(totalViews, "view")}</span>
+      </p>
+    </div>
+  );
+}
