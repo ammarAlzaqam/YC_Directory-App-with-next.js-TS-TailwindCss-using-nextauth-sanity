d0b7ecf3459b5c346d7f8fa23fa36e207d2fc541
ammarAlzaqam <git@ammaralzaqam.com>
2025-08-17 10:45:01 +0300

    playlist schema

--- studio-jsm-yc-directory/schemaTypes/index.ts
+++ studio-jsm-yc-directory/schemaTypes/index.ts
@@ -1,4 +1,5 @@
 import {authorType} from './author'
+import {playlistType} from './playlist'
 import {startupType} from './startup'
 
-export const schemaTypes = [authorType, startupType]
+export const schemaTypes = [authorType, startupType, playlistType]
